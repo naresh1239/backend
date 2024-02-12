94354ea041c5a2747ae1641df809d73e80b2aa2e
Naresh <iamnareshkumawat1@gmail.com>
2024-02-12 16:14:58 +0530

    new

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-// Import required modules
-const express = require('express');
-
-// Create an instance of Express
+const express = require("express");
 const app = express();
-
-// Define routes
-app.get('/', (req, res) => {
-  res.send('Welcome to the homepage!');
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
-
-app.get('/about', (req, res) => {
-  res.send('This is the about page.');
-});
-
-app.get('/contact', (req, res) => {
-  res.send('Contact us at example@example.com');
-});
-
-// Handle 404 - Not Found
-app.use((req, res) => {
-  res.status(404).send('404 Not Found');
-});
-
-// Start the server on port 3000
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
